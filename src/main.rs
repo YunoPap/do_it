78395ea3743b10a106c077687e895e0cdfbab934
Yuno <stepevade23@gmail.com>
2026-05-12 23:08:34 -0400

    Changed where main.rs is

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Result;
+use ratatui::{
+    crossterm::event::{self, Event}, 
+    widgets::{Paragraph, Widget},
+    DefaultTerminal, Frame,
+};
+
+#[derive(Debug, Default)]
+struct AppState {
+    items: Vec<TodoItem>,
+}
+
+#[derive(Debug, Default)]
+struct TodoItem {
+    is_done: bool, 
+    description: String, 
+}
+
+fn main() -> Result<()> {
+    let mut state = AppState::default();    
+    color_eyre::install()?;
+    
+    let terminal = ratatui::init();
+    let result = run(terminal, &mut state);
+    
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal, app_state:&mut AppState) -> Result<()> {
+    loop {
+        //Rendering 
+         terminal.draw(|f| render(f,app_state))?;
+        //Input handling
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                event::KeyCode::Esc => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+        
+    }
+    Ok(())
+}
+
+fn render(frame: &mut Frame, app_state:&mut AppState) {
+    Paragraph::new("Hello from application").render(frame.area(), frame.buffer_mut());
 }
