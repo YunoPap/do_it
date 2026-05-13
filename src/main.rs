a694f6a4ded2cebb20ffcd21da7ab35acb5aa941
Yuno <stepevade23@gmail.com>
2026-05-13 02:43:37 -0400

    added 'delete button'

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use ratatui::{
     crossterm::event::{self, Event},
     layout::{Constraint, Layout},
     prelude::Stylize,
-    style::Color,
-    widgets::{Block, BorderType, List, ListItem, Widget},
+    style::{Color, Style},
+    widgets::{Block, BorderType, List, ListItem, ListState, Widget},
     DefaultTerminal, Frame,
 };
 
 #[derive(Debug, Default)]
 struct AppState {
     items: Vec<TodoItem>,
+    list_state: ListState,
 }
 
 #[derive(Debug, Default)]
@@ -23,15 +24,15 @@ fn main() -> Result<()> {
     let mut state = AppState::default();
     state.items.push(TodoItem { 
         is_done: false, 
-        description: String::from("Finish application"),
+        description: String::from("Description 1"),
     });
     state.items.push(TodoItem { 
         is_done: false, 
-        description: String::from("Finish application"),
+        description: String::from("Description 2"),
     });
     state.items.push(TodoItem { 
         is_done: false, 
-        description: String::from("Finish application"),
+        description: String::from("Description 3"),
     });
     color_eyre::install()?;
     
@@ -52,6 +53,22 @@ fn run(mut terminal: DefaultTerminal, app_state:&mut AppState) -> Result<()> {
                 event::KeyCode::Esc => {
                     break;
                 }
+                event::KeyCode::Char(char) => {
+                    match char {
+                        'D' => {
+                            if let Some(index) = app_state.list_state.selected() {
+                                app_state.items.remove(index);
+                            }
+                        },
+                        'k' => {
+                            app_state.list_state.select_previous();
+                        },
+                        'j' => {
+                            app_state.list_state.select_next();
+                        },
+                        _ => {}
+                    };
+                }
                 _ => {}
             }
         }
@@ -60,7 +77,7 @@ fn run(mut terminal: DefaultTerminal, app_state:&mut AppState) -> Result<()> {
     Ok(())
 }
 
-fn render(frame: &mut Frame, app_state:&mut AppState) {
+fn render(frame: &mut Frame, app_state: &mut AppState) {
     let border_area = Layout::vertical([Constraint::Fill(1)])
         .margin(1)
         .split(frame.area())[0];
@@ -74,11 +91,14 @@ fn render(frame: &mut Frame, app_state:&mut AppState) {
         .fg(Color::Yellow)
         .render(border_area, frame.buffer_mut());
 
-    List::new(
+    let list = List::new(
         app_state 
         .items
         .iter()
         .map(|x| ListItem::from(x.description.clone()))
     )
-    .render(inner_area, frame.buffer_mut());
+    .highlight_symbol(">")
+    .highlight_style(Style::default().fg(Color::Green));
+    
+    frame.render_stateful_widget(list, inner_area, &mut app_state.list_state);
 }
